fe3238df0ac25341c5c89ccaff0e6d2cf9ea154e
Sudip Limbu <4slimbu@gmail.com>
2020-05-09 23:58:54 +0545

    Temp Commit

--- limbook_api/__init__.py
+++ limbook_api/__init__.py
@@ -14,8 +14,10 @@ def create_app(config_class=Config):
     setup_db(app)
 
     from limbook_api.main.routes import main
+    from limbook_api.posts.routes import posts
     from limbook_api.errors.handlers import errors
     app.register_blueprint(main)
+    app.register_blueprint(posts)
     app.register_blueprint(errors)
 
     return app
--- limbook_api/posts/routes.py
+++ limbook_api/posts/routes.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, abort
+
+from limbook_api.db.main import Post
 
 posts = Blueprint('posts', __name__)
 
 
-@posts.route("/")
+# ====================================
+# ROUTES
+# ====================================
+@posts.route("/", methods=['GET'])
 def get_posts():
-    return render_template('home.html')
+    """ Get all available posts
+
+        Returns:
+            success (boolean)
+            posts (list)
+            total_posts (int)
+    """
+    try:
+        # get posts
+        posts = Post.query.all()
+        # get count
+        posts_count = Post.query.count()
+
+        # format
+        data = []
+        for drink in posts:
+            data.append(drink.short())
+
+        # return the result
+        return jsonify({
+            'success': True,
+            'posts': data,
+            'posts_count': posts_count
+        })
+    except Exception:
+        abort(400)
