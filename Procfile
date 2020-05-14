bda4594f30b8a5a99436ac2ee81082e6c2f22c3c
Sudip Limbu <4slimbu@gmail.com>
2020-05-14 23:40:55 +0545

    Style: Clean up

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn --bind 0.0.0.0:$PORT run:app
+
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from limbook_api import create_app, Config
 
+# Create flask app
 app = create_app(Config)
 
 if __name__ == '__main__':
