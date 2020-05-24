196ac06f6625b41e0a07bb851241a17fa7bc947a
Sudip Limbu <4slimbu@gmail.com>
2020-05-24 22:50:46 +0545

    Refactor: Comments module

--- tests/v1/test_posts.py
+++ tests/v1/test_posts.py
@@ -3,7 +3,7 @@ from unittest import main
 from flask import json
 
 from limbook_api.v1.image_manager import generate_image, Image
-from limbook_api.v1.posts import generate_post, Post
+from limbook_api.v1.posts import generate_post
 from tests.base import BaseTestCase, test_user_id, api_base
 
 
